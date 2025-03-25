da453b0156a3195102ee73d0a49ac3abe7559b68
Joel Wembo <joelwembo@outlook.com>
2025-03-26 01:18:25 +0800

    added new script hcl

---
+++ Cloud/upcloud/.terraform.lock.hcl
@@ -0,0 +1,23 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/upcloudltd/upcloud" {
+  version     = "2.4.0"
+  constraints = "2.4.0"
+  hashes = [
+    "h1:Nkcgom+3nlreEN+SXyAkSNzhwb/SFPAfZ7iwfZgrnaU=",
+    "zh:1161197c7849e3fbbd31d1471f141ad240280e8d1621fdda352cd503ddcce5b7",
+    "zh:19ba1a4373b7a220b8294c1694944b3a5702429d6dc7b4f6fe25393b514d619b",
+    "zh:1ebaeba42d6d57eadfbeb6b2987c66c7fadbfe2a8a39f4828fc5bf711a991201",
+    "zh:20d54ffa25015a3dd83bcc2dbccebcd0398b6af8d11aaa3f23e3eba9d75825f3",
+    "zh:368dd080f56e4e9d29499eb5e3bd8619549345ef9c0dfc2781e0fe67a394f34b",
+    "zh:4a6347e033e29e514b09e3d5ecc17f677f966f08e2be2e6159def3eaf1da3368",
+    "zh:57ff6dea39429ffba3afc12a0311b6eb90a595c739600f7dfcee875530c0fcf5",
+    "zh:68a8293b28e1fe6aee9f78f467d51cde45de227eccd74d7ed0275d0bab0e0822",
+    "zh:786d2478486a8498f9e46d61d00f89f778f10a6dd49a79a94b9e80a89d8da009",
+    "zh:b947fe84edaf30e34d891be3fdf0140ba80db9adedffd677ecb970222614c22b",
+    "zh:bd7e8ce961433d7b429bb2d82cad652180ea13252efcd99fb422ddb77305283f",
+    "zh:c0921b7f39dbe9a32f63688ca63f153d9a5e56c47a76a9a5e0e8ef38e1e8a118",
+    "zh:dd5bb954c41f6de8535ef95ee68467f567e3bdec97736924561e2ab54a3c0942",
+  ]
+}
---
+++ Cloud/upcloud/main.tf
@@ -0,0 +1,73 @@
+# main.tf
+terraform {
+  required_providers {
+    upcloud = {
+      source = "upcloudltd/upcloud"
+      version = "2.4.0"
+    }
+  }
+}
+
+
+
+# Create a network for the Kubernetes cluster
+resource "upcloud_network" "example" {
+  name = "example-network"
+  zone = "de-fra1"
+  ip_network {
+    address = "172.16.1.0/24"
+    dhcp    = true
+    family  = "IPv4"
+  }
+
+  # UpCloud Kubernetes Service will add a router to this network to ensure cluster networking is working as intended.
+  # You need to ignore changes to it, otherwise TF will attempt to detach the router on subsequent applies
+  lifecycle {
+    ignore_changes = [router]
+  }
+}
+
+# Create a Kubernetes cluster
+resource "upcloud_kubernetes_cluster" "example" {
+  # Allow access to the cluster control plane from any external source.
+  control_plane_ip_filter = ["0.0.0.0/0"]
+  name                    = "exampleapp"
+  network                 = upcloud_network.example.id
+  zone                    = "de-fra1"
+}
+
+# Kubernetes cluster with private node groups requires a network that is routed through NAT gateway.
+resource "upcloud_router" "example2" {
+  name = "example2-router"
+}
+
+resource "upcloud_gateway" "example2" {
+  name     = "example2-nat-gateway"
+  zone     = "de-fra1"
+  features = ["nat"]
+
+  router {
+    id = upcloud_router.example2.id
+  }
+}
+
+resource "upcloud_network" "example2" {
+  name = "example2-network"
+  zone = "de-fra1"
+  ip_network {
+    address            = "10.10.0.0/24"
+    dhcp               = true
+    family             = "IPv4"
+    dhcp_default_route = true
+  }
+  router = upcloud_router.example2.id
+}
+
+resource "upcloud_kubernetes_cluster" "example2" {
+  name                    = "prodxcloud-cluster-prod"
+  network                 = upcloud_network.example2.id
+  zone                    = "de-fra1"
+  plan                    = "production-small"
+  private_node_groups     = true
+  control_plane_ip_filter = ["0.0.0.0/0"]
+}
